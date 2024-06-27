5e2e406255f51ab566f65353b0d7aed7122701b9
JimcostDev <rjak727@gmail.com>
2024-06-27 16:56:18 +0200

    header y skills completados

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,17 +3,22 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			backgroundImage: {
+				BACKGROUND_DESKTOP: "url('/BACKGROUND-DESKTOP.webp')",
+				BACKGROUND_MOBILE: "url('/BACKGROUND_MOBILE.webp')",
+				BACKGROUND_PROJECT: "url('/BACKGROUND_PROJECT.webp')",
+			},
 			backgroundColor: (theme) => ({
 				...theme("colors"),
 				primary: "#03A9F4",
 				secondary: "#F2FBFF",
 				tertiary: "#00142E",
-			  }),
+			}),
 			  textColor: {
 				primary: "#03A9F4",
 				secondary: "#F2FBFF",
 				tertiary: "#00142E",
-			  },
+			},
 		},
 	},
 	plugins: [],
