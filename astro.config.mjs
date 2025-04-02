a0181046a38e4e6578dab1583127e291393bc53d
JimcostDev <rjak727@gmail.com>
2025-04-02 21:09:45 +0200

    actualización a tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [],
   output: 'server',
   adapter: vercel(),
 });
